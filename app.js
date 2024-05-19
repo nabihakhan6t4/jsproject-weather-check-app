5cb4fcc592672fddcbdf285b5ae742d9cce4364a
nabihakhan6t4 <khannabiha923@gmail.com>
2024-05-19 17:58:45 +0300

    project-almost-complete

---
+++ app.js
@@ -0,0 +1,108 @@
+var weather = prompt("Enter your area weather:");
+
+if (weather == 'hot') {
+    document.write('<div class="card w-50 m-auto " style="width: 18rem;">' +
+    '<img src="images/hot.jpeg" class="card-img-top rounded-5" alt="...">' +
+   ' <div class="card-body ">' +
+    '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+   ' </div>' +
+ ' </div>');
+} else {
+    document.write(" 😜 Are you kidding me?");
+}
+
+
+if (weather == 'cold') {
+    document.write('<div class="card w-50 m-auto " style="width: 18rem;">' +
+    '<img src="images/cold.jpeg" class="card-img-top rounded-5" alt="...">' +
+   ' <div class="card-body ">' +
+    '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+   ' </div>' +
+ ' </div>');
+} else {
+    document.write(" 😜 Are you kidding me?");
+}
+
+
+if (weather == 'sunny') {
+    document.write('<div class="card w-50 m-auto " style="width: 18rem;">' +
+    '<img src="images/sunny.jpeg" class="card-img-top rounded-5" alt="...">' +
+   ' <div class="card-body ">' +
+    '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+   ' </div>' +
+ ' </div>');
+} else {
+    document.write(" 😜 Are you kidding me?");
+}
+
+
+
+
+if (weather == 'cloudy') {
+    document.write('<div class="card w-50 m-auto " style="width: 18rem;">' +
+    '<img src="images/cloudy.jpeg" class="card-img-top rounded-5" alt="...">' +
+   ' <div class="card-body ">' +
+    '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+   ' </div>' +
+ ' </div>');
+} else {
+    document.write(" 😜 Are you kidding me?");
+}
+
+if (weather == 'partly') {
+    document.write('<div class="card w-50 m-auto " style="width: 18rem;">' +
+    '<img src="images/partly .jpeg" class="card-img-top rounded-5" alt="...">' +
+   ' <div class="card-body ">' +
+    '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+   ' </div>' +
+ ' </div>');
+} else {
+    document.write(" 😜 Are you kidding me?");
+}
+
+if (weather == 'rainy') {
+    document.write('<div class="card w-50 m-auto " style="width: 18rem;">' +
+    '<img src="images/rainy.jpeg" class="card-img-top rounded-5" alt="...">' +
+   ' <div class="card-body ">' +
+    '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+   ' </div>' +
+ ' </div>');
+} else {
+    document.write(" 😜 Are you kidding me?");
+}
+
+
+
+if (weather == 'stormy') {
+    document.write('<div class="card w-50 m-auto " style="width: 18rem;">' +
+    '<img src="images/stormy.jpeg" class="card-img-top rounded-5" alt="...">' +
+   ' <div class="card-body ">' +
+    '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+   ' </div>' +
+ ' </div>');
+} else {
+    document.write(" 😜 Are you kidding me?");
+}
+
+if (weather == 'windy') {
+    document.write('<div class="card w-50 m-auto " style="width: 18rem;">' +
+    '<img src="images/windy.jpeg" class="card-img-top rounded-5" alt="...">' +
+   ' <div class="card-body ">' +
+    '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+   ' </div>' +
+ ' </div>');
+} else {
+    document.write(" 😜 Are you kidding me?");
+}
+
+if (weather == 'snowy') {
+    document.write('<div class="card w-50 m-auto " style="width: 18rem;">' +
+    '<img src="images/snowy.jpeg" class="card-img-top rounded-5" alt="...">' +
+   ' <div class="card-body ">' +
+    '  <h1 class="card-text text-center">' + 'its ' + weather +  ' today' + '</h1>' +
+   ' </div>' +
+ ' </div>');
+} else {
+    document.write(" 😜 Are you kidding me?");
+}
+
